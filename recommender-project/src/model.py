4bbe3897ff9712fc16b7dfc6aafdef7bba0b8845
Kin Hong Tan <118425474+Kin-Hong-Tan@users.noreply.github.com>
2025-03-26 14:25:18 +0800

    Delete recommender-project directory

--- recommender-project/src/model.py
+++
@@ -1,6 +0,0 @@
-from lightfm import LightFM
-
-def train_lightfm_model(interaction_matrix, loss='warp', epochs=30):
-    model = LightFM(loss=loss)
-    model.fit(interaction_matrix, epochs=epochs)
-    return model
