7179aaad311a0b305357cc565b1ec31be30f6b73
Kin Hong Tan <118425474+Kin-Hong-Tan@users.noreply.github.com>
2025-04-22 19:00:16 +0800

    Update dag_digibank_analytics.py

--- GXBank-Portfolio/airflow_dags/dag_digibank_analytics.py
+++ GXBank-Portfolio/airflow_dags/dag_digibank_analytics.py
@@ -2,6 +2,14 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.utils.dates import days_ago
 
+default_arguments = {
+'owner': 'Kin Hong',
+'email': 'khtan47@outlook.com',
+'start_date': datetime(2025, 4, 20)
+}
+
+with DAG('etl_workflow', default_args=default_arguments) as etl_dag:
+
 with DAG(dag_id='digibank_dashboard_etl',
          start_date=days_ago(1),
          schedule_interval='@daily',
